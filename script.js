59d96174dbd85412221f8610bfcc09477f6da7db
Avibtb <avi.singh.iit01@gmail.com>
2021-06-11 00:47:54 +0545

    added delete button

--- script.js
+++ script.js
@@ -32,14 +32,15 @@ let time = setInterval(function(){
     runningTime.textContent = Zero(hours) + ":" + Zero(minutes) + ":" + Zero(seconds) + " " + meridian;
 },1000);
 
-// adding to the seconds 
-function addSec(){
-    let sec = 59;
-    for(let i = 0 ; i<= sec; i++){
-        second.options[second.options.length] = new Option(Zero(i),i);
+
+//adding to the hours
+function addHr(){
+    let hr = 12;
+    for(let i =0 ;i <=hr;i++){
+        hour.options[hour.options.length] = new Option(Zero(i),i);
     }
 }
-addSec();
+addHr();
 
 //adding to the minutes
 function addMin(){
@@ -51,20 +52,26 @@ function addMin(){
 }
 addMin();
 
-//adding to the hours
-function addHr(){
-    let hr = 12;
-    for(let i =1 ;i <=hour;i++){
-        hour.options[hour.options.length] = new Option(Zero(i),i);
+
+
+// adding to the seconds 
+function addSec(){
+    let sec = 59;
+    for(let i = 0 ; i<= sec; i++){
+        second.options[second.options.length] = new Option(Zero(i),i);
     }
 }
-addHr();
+addSec();
 
-//adding the list of alarm and settint it
+showAlarms();
+triggerAlarm();
+
+
+//adding the list of alarm and setting it
 
 setAlarm.addEventListener('click',function(){
     let Hour = hour.value;
-    let Minutte = minute.value;
+    let Minute = minute.value;
     let Seconds = second.value;
     let Meridian = meridian.value;
     let Lablel = alarmLabel.value;
@@ -73,5 +80,110 @@ setAlarm.addEventListener('click',function(){
         Lablel = "Alarm"
     }
 
+    //alarm time 
+    let Time = Zero(Hour) + ":" + Zero(Minute) + ":" + Zero(Seconds)  + " " + Meridian;
+
+    let alarmEvent = {
+        label : Lablel,
+        time: Time
+    }
+
+    let nextAlarm = localStorage.getItem('nextAlarm');
+
+    if(nextAlarm == null){
+        list = [];
+    }else{
+        list = JSON.parse(nextAlarm);
+    }
+
+    list.push(alarmEvent);
+    localStorage.setItem('nextAlarm', JSON.stringify(list));
+
+    showAlarms();
+    triggerAlarm();
+});
+
+// deleting alarm 
+function deleteAlarm(index){
+    let nextAlarm = localStorage.getItem('nextAlarm');
+    let list = JSON.parse(nextAlarm);
+
+    list.splice(index,1);
+
+    localStorage.setItem('nextAlarm',JSON.stringify(list));
+
+    showAlarms();
+    triggerAlarm();
     
-})
+}
+// showing next Alarm 
+function showAlarms(){
+    let nextAlarm =localStorage.getItem('nextAlarm');
+
+    if(nextAlarm == null){
+        list = [];
+    }else{
+        list =JSON.parse(nextAlarm);
+    }
+    let html = '';
+    let nextAlarmsList = document.getElementById('all-alarm-list');
+
+    nextAlarmsList.innerHTML = html;
+
+    list.forEach((alarmEvent,index) =>{
+        html += `<tr>
+                    <td class="alarm-list-info">
+                        <p style="margin: 5px;">${alarmEvent.label}</p>
+                        <p><i class="far fa-clock"></i> ${alarmEvent.time}</p>
+                    </td>
+                    <td class="del_btn" onclick="deleteAlarm(${index})"><i class="fas fa-trash"></i></td>
+                </tr>`
+    });
+
+    nextAlarmsList.innerHTML = html;
+}
+
+
+//triggering the alarm
+function triggerAlarm(){
+    let nextAlarm =localStorage.getItem('nextAlarm');
+
+    if(nextAlarm == null){
+        list = [];
+    }else{
+        list =JSON.parse(nextAlarm);
+    }
+
+    if(list.length != 0){
+        let newAlarm = list[0];
+      
+    
+
+
+    setInterval(function(){
+            let date = new Date(); 
+            let hours = date.getHours(); 
+            let minutes = date.getMinutes(); 
+            let seconds = date.getSeconds(); 
+            let meridian = (date.getHours()) < 12 ? 'AM' : 'PM'; 
+        
+          
+            if(hours <= 12){
+                hours = hours 
+            }else{
+                hours = hours - 12;
+            }
+        
+            let running_Time = runningTime.textContent = Zero(hours) + ":" + Zero(minutes) + ":" + Zero(seconds) + "  " + meridian;
+
+            if(newAlarm.time == running_Time){
+                window.alert(newAlarm.label);
+                list.splice(0,1);
+                localStorage.setItem('nextAlarm',JSON.stringify(list));
+                showAlarms();
+                triggerAlarm();
+            }
+        },1000);
+}
+}
+
